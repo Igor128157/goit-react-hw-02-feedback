6c7815b0fdc8c16d42fbd1eeb52b667decbdc730
Igor Verovka <igor0063@ukr.net>
2020-05-25 12:22:49 +0300

    Updates

---
+++ precache-manifest.e7ca896a0704ea0f26747e62c2d445d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bd585301fc30575e19967afb73a942",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "e7a9345d001c556193cd",
+    "url": "/goit-react-hw-02-feedback/static/css/main.cb3efccd.chunk.css"
+  },
+  {
+    "revision": "51aea965943e6f560839",
+    "url": "/goit-react-hw-02-feedback/static/js/2.bd1b6820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.bd1b6820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a9345d001c556193cd",
+    "url": "/goit-react-hw-02-feedback/static/js/main.3f8bba4e.chunk.js"
+  },
+  {
+    "revision": "67fc603d4fe28264fbd4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.56a9b6a1.js"
+  }
+]);
